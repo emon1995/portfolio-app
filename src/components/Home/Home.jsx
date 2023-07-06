110f314e27f0d41d2772f18813a8536f98db11f3
emon1995 <emonhossain995@gmail.com>
2023-07-06 12:28:36 +0600

    home some added

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,14 @@
+
+
+const Home = () => {
+    return (
+        <div className="home">
+            <div className="home-info">
+                <h1>Hey, I&apos;m Emon Hossain</h1>
+                <p>A frontend-focused web developer creates the user interface of websites and web applications, which promotes the success of the final product.</p>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
